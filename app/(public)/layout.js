c17b069785f4f28e566a5be352cfa19162c6f4b7
Afrina Tithy <afrina.tithy@gmail.com>
2025-12-25 11:52:24 +0600

    setup public layout with navbar and footer

--- app/(public)/layout.js
+++ app/(public)/layout.js
@@ -7,10 +7,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body >
-        {children}
-      </body>
-    </html>
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
   );
 }
