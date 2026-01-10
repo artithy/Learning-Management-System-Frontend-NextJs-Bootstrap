e0ad10c0ed77390aca17a20ac4974d44a204f455
Afrina Tithy <afrina.tithy@gmail.com>
2026-01-10 23:10:49 +0600

    Add top margin to CourseGrid

--- app/student-panel/page.js
+++ app/student-panel/page.js
@@ -4,7 +4,7 @@ import CourseGrid from "@/components/courseGrid";
 export default function StudentDashboard() {
     return (
         <>
-            <div className="container-fluid bg-light min-vh-100 pt-4">
+            <div className="container-fluid bg-light min-vh-100 pt-4 mt-5" >
                 <CourseGrid showCategories={true} showEnrolledOnly={true} />
             </div>
         </>
